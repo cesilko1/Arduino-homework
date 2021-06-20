c7df85ad5e05c9d70e5fb7f897ee12d43d486a6f
Vilém Raška <wilhelm.master@gmail.com>
2021-06-20 17:16:55 +0200

    8led code added

---
+++ 8led/8led.ino
@@ -0,0 +1,31 @@
+int rychlost = 500;
+byte ledky[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+
+void setup() {
+  for(byte i=2; i<=9; i++) {
+  	pinMode(i, OUTPUT); 
+  }
+}
+
+void loop() {
+  for(byte i=2; i<=9; i++) {
+    zhasnout();
+    digitalWrite(i, LOW);
+    delay(rychlost);
+  }
+  
+  delay(rychlost);
+  
+  for(byte i=9; i>=2; i--) {
+    zhasnout();
+    digitalWrite(i, LOW);
+    delay(rychlost);
+  }
+}
+
+void zhasnout() {
+  for(byte i=2; i<=9; i++) {
+    digitalWrite(i, HIGH);
+  }
+}    
---
+++ 8led/README.md
@@ -0,0 +1,1 @@
+#Dokončete v Tinkercadu a zašlete odkaz ke kontrole - běžící světlo 8x LED
